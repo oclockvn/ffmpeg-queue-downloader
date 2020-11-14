b0eddd97a09b46a76f81bc60f868c77f6f020c93
Quang Phan <oclockvn@gmail.com>
2020-11-14 15:53:26 +0700

    update download format

--- QffmpegDownloader/Program.cs
+++ QffmpegDownloader/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace QffmpegDownloader
@@ -15,72 +16,114 @@ namespace QffmpegDownloader
                 .AddJsonFile("appsettings.json", true, true)
                 .Build();
 
-            var delay = config.GetValue<int>("Delay", 3);
-            var start = config.GetValue<int>("Start", 0);
+            var delay = config.GetValue<int>("Delay", 3); // in minutes
+            var input = config.GetValue<string>("Input", "playlist.txt"); // playlist name, default is playlist.txt
 
-            Console.WriteLine($"> Configuration: start = {start}, delay = {delay} (minutes)");
+            var currDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var inputFile = Path.Combine(currDir, input);
 
-            var files = Directory.GetFiles(".", "**.bat"); // looking for .bat files
-
-            if (files == null || files.Length == 0)
+            if (!File.Exists(inputFile))
             {
-                Console.WriteLine("No bat files found. Press enter to exit...");
-                Console.ReadLine();
+                Console.WriteLine("Download list not found, please add a playlist.txt or read the instruction. Press any key to exit...");
+                Console.ReadKey();
                 return;
             }
 
-            var list = files.OrderBy(x => x).ToList();
-            var eps = list.Select(l => GetEpNumber(l)).ToList();
-            var latestEp = eps.Max();
-
-            if (latestEp < 0)
+            var links = File.ReadAllLines(inputFile).Where(l => !string.IsNullOrWhiteSpace(l)).ToArray();
+            if (links == null || links.Length == 0)
             {
-                Console.WriteLine("Invalid bat name. Press enter to exit...");
-                Console.ReadLine();
                 return;
             }
 
-            while (start <= latestEp)
-            {
-                var bat = $"{start}.bat"; // = 2.bat
-                var result = $"E{start}.mp4"; // = E2.mp4 
+            var total = links.Length;
+            var output = string.Empty;
+            var startEpisode = 1; // start chapter, default is chapter 1
+            var startIndex = 0;
 
-                if (eps.Contains(start) && !File.Exists(result))
+            // if first line is a ffmpeg command already, no need to create the output folder
+            var firstLine = links[0];
+            if (!firstLine.StartsWith("ffmpeg "))
+            {
+                // if first line is a link, the use default output
+                if (firstLine.StartsWith("http"))
                 {
-                    Console.WriteLine($"> {bat} downloading...");
-                    Process.Start(new ProcessStartInfo
-                    {
-                        FileName = bat,
-                        CreateNoWindow = false,
-                        WindowStyle = ProcessWindowStyle.Normal,
-                        UseShellExecute = true,
-                    });
-                    await Task.Delay(delay * 1000 * 60);
+                    firstLine = "output 1";
+                }
+                else
+                {
+                    startIndex = 1;// first line is the info line, so skip it from total download
                 }
 
-                start++;
+                // first line format
+                // output-folder ep-number
+                var info = firstLine.Split(" ");
+                output = info[0];
+
+                if (info.Length == 2)
+                {
+                    if (int.TryParse(info[1], out var temp))
+                    {
+                        startEpisode = temp;
+                    }
+                }
+
+                var outputDir = Path.Combine(currDir, output);
+
+                try
+                {
+                    if (!Directory.Exists(output))
+                    {
+                        Directory.CreateDirectory(output);
+                    }
+                }
+                catch
+                {
+                    return; // cannot create folder for some reason, stop program
+                }
             }
 
-            Console.WriteLine("Done! Press enter to exit...");
-            Console.ReadLine();
+            for (var i = startIndex; i < total; i++)
+            {
+                var actualLink = BuildArgument(links[i], output, startEpisode);
+                Debug.WriteLine($"> Downloading {actualLink}");
+
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "ffmpeg",
+                    Arguments = actualLink,
+                    CreateNoWindow = false,
+                    WindowStyle = ProcessWindowStyle.Normal,
+                    UseShellExecute = true,
+                });
+
+                // delay for next download
+                await Task.Delay(delay * 1000 * 60);
+
+                startEpisode++;
+            }
+
+            // Console.WriteLine("Done! Press enter to exit...");
+            // Console.ReadLine();
         }
 
         /// <summary>
-        /// get the number from filename.bat
+        /// If link is not a ffmpeg link then format it regardless the correct format, otherwise return as is
         /// </summary>
-        /// <param name="name">015.bat</param>
-        /// <returns>15</returns>
-        static int GetEpNumber(string name)
+        /// <param name="link">The link</param>
+        /// <returns></returns>
+        static string BuildArgument(string link, string output, int ep = 1)
         {
-            if (string.IsNullOrWhiteSpace(name))
-                return -1;
+            if (string.IsNullOrWhiteSpace(link))
+            {
+                return string.Empty;
+            }
 
-            var nameonly = Path.GetFileNameWithoutExtension(name);
-            //var names = name.Split('.');
-            if (int.TryParse(nameonly, out var ep) && ep > 0)
-                return ep;
+            if (link.StartsWith("ffmpeg"))
+            {
+                return link[6..]; // remove ffmpeg as we'll use it as the filename for the process
+            }
 
-            return -1;
+            return $"-i {link} -map p:2? -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 {output}/ep-{ep}.mp4";
         }
     }
 }
